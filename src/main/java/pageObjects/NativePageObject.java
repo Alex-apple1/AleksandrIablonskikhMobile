98a0ef6a9500e29adc2da518760b160d244a918b
Aleksandr Iablonskikh <Aleksandr_Iablonskikh@epam.com>
2021-09-30 17:24:11 +0300

    full version of native part of hw2

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -8,12 +8,40 @@ import org.openqa.selenium.support.PageFactory;
 
 public class NativePageObject  {
 
+//    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
+//    WebElement signInBtn;
+
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement registrationButton;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement registrationEmail;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement registrationUserName;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement registrationPassword;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement registrationConfirmPassword;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement registrationNewAccountButton;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
+    WebElement loginEmailField;
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
+    WebElement passwordField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/action_bar")
+    WebElement assertName;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/add_new_expense")
+    WebElement addNewExpenseButton;
 
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements( new AppiumFieldDecorator(appiumDriver), this);
     }
+
+
 
 
 }
