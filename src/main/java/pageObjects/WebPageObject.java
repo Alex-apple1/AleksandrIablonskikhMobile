2f4fdd40d4a64ec699694e5b3bb4c5f7e72afefe
Aleksandr Iablonskikh <Aleksandr_Iablonskikh@epam.com>
2021-10-01 11:52:16 +0300

    some progress

--- src/main/java/pageObjects/WebPageObject.java
+++ src/main/java/pageObjects/WebPageObject.java
@@ -1,14 +1,22 @@
 package pageObjects;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class WebPageObject  {
+
+    final String searchData = "EPAM";
+
+    @AndroidFindBy(xpath = "//*[@name='q']")
+    WebElement googleSearchField;
+    @AndroidFindBy(xpath = "//*[@id='input']")
+    WebElement googleSearchButton;
+
 
     public WebPageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(appiumDriver, this);
 
     }
-
-
 }
